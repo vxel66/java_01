c96be82bfe904d134f6253c14f9cbd4bc6e353e2
minwook <minwook@220.74.92.181>
2021-10-15 00:10:21 +0900

    123

--- java_lmw/src/연습/Ab.java
+++ java_lmw/src/연습/Ab.java
@@ -2,11 +2,18 @@ package ¿¬½À;
 
 public class Ab {
 	
-	private String booklist ;
+	String booklist ;
+	private	int booknum ;
 
 	public Ab() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Ab(int booknum) {
+		
+		this.booknum = booknum;
+	}
+	
 	
 	public Ab(String booklist) {
 		
@@ -19,6 +26,14 @@ public class Ab {
 
 	public void setBooklist(String booklist) {
 		this.booklist = booklist;
+	}
+
+	public int getBooknum() {
+		return booknum;
+	}
+
+	public void setBooknum(int booknum) {
+		this.booknum = booknum;
 	}
 	
 	
