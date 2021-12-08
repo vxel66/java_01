a45be941a248c1aba4f89e3dae4b4081e307bac2
vxel66 <91528966+vxel66@users.noreply.github.com>
2021-12-08 10:21:54 +0900

    Delete sample directory

--- sample/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module sample {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.sql;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	opens controller to javafx.graphics, javafx.fxml;
-	
-}
